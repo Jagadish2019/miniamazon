c410940feaa0b8ce14c3585e31a72a215a222502
Jagadish2019 <jagadishg2019@gmail.com>
2019-03-17 16:28:37 +0530

    mini-amazon, python and jinja2

--- api.py
+++ api.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
 @app.route('/') # decorator
 def home():
 	# return "hello world"
-	return render_template('home.html',title="a2z products",name="Developed by Jagan")
+	return render_template('home.html',title="a2z products",name="Developed by Jagan",home = 'home')
 
 @app.route('/about') # decorator
 def about():
@@ -15,7 +15,26 @@ def about():
 @app.route('/contact')
 def contact():
 	# return "Python is a interpreter, My name is Jagadish and I am a Python geek"
-	return render_template('contact.html')
+	return render_template('contact.html', title='contact')
+
+@app.route('/welcome')
+def welcome():
+	return render_template('welcome.html')
+
+@app.route('/login',methods = ['POST'])
+def login():
+
+	user = {'username':'jagan','password':'12345'}
+
+	username = request.form['username']
+	password = request.form['password']
+
+	if user['username'] == username:
+		if user['password'] == password:
+			return redirect(url_for('welcome'))
+		return "wrong password, go back and try again!"
+	return "this user doesn't exist. Go back and enter a valid user"
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
